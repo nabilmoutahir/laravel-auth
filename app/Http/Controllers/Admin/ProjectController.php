be6544952dcaa1dc71fc29eb6aa3df0a51e63461
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-04-08 23:14:47 +0200

    add destroy function

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -85,6 +85,8 @@ class ProjectController extends Controller
      */
     public function destroy(Project $project)
     {
-        //
+        $project->delete();
+
+        return redirect()->route('admin.projects.index');
     }
 }
