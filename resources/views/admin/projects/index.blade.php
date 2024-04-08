1c99aa2525d862bc0a3bac014747fb1377085902
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-04-08 22:52:53 +0200

    add index table

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -6,7 +6,32 @@
         <div class="container">
             <h1>Projects</h1>
 
-            @dump($projects)
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Slug</th>
+                        <th>Content</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($projects as $project)
+                        <tr>
+                            <td>{{ $project->id }}</td>
+                            <td>{{ $project->title}}</td>
+                            <td>{{ $project->slug}}</td>
+                            <td>{{ $project->content}}</td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td>Not Found</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+
+            {{$projects->links()}}
         </div>
 
     </section>
